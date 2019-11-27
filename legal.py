bec74a6096885eb405fea756678ae879317cf727
vasu <vasutomar26@gmail.com>
2019-11-27 09:06:35 +0530

    Beautify legal.py

--- legal.py
+++ legal.py
@@ -24,15 +24,25 @@ def main():
    tokens = word_tokenize(final_body)
    stop_words = set(stopwords.words('english'))
    filtered_words = [w for w in tokens if w not in stop_words]
-   
    # print(filtered_words)
+
+   lem = WordNetLemmatizer()
+   stem = PorterStemmer()
+
+   for word in filtered_words:
+      if len(word)>1:
+         print('For',word,': '+lem.lemmatize(word,"v"),stem.stem(word))
 
    ''' CBOW Model '''
    model = gensim.models.Word2Vec([filtered_words], min_count = 1, size = 100, window = 5) 
    model.train([filtered_words],total_examples=1,epochs=1)
    word_vector = model.wv
    word_vector.save('vector.kv')
-   print('Word vector for justice is : \n'+str(model['justice']))
+   while True:
+      print('Enter the word : ')
+      word = input()
+      print(model[word])
+      print('--------------------------')
 
 if __name__ == "__main__":
   main()
